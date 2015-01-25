eece05503e338292d85f914c15ee9ef337d742a7
Nick Abboud <nabboud@gmail.com>
2015-01-24 21:00:39 -0700

    Added a basic NPC object

---
+++ GGJ/obj.npc.js
@@ -0,0 +1,9 @@
+function NPC(node) {
+  this.node = $(node);
+
+  // updates the position of the enemy
+  this.updateX = function(npcNode){
+    var shift = Math.floor((Math.random() * 2) + 1) - 1;
+    this.node.X(shift, true);
+  };  
+}
